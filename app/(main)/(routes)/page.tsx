c61c6e362ab4c2aaf45e045baba84ba57dd229bf
ycFan <fyc5860@gmail.com>
2023-08-29 01:27:20 +0800

    second commit

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <p className='text-3xl font-bold text-indigo-500'>
+        Hello Discord
+      </p>
+      <Button>click me</Button>
+    </div>
+
+  )
+}
