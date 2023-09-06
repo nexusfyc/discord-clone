39a236cd0fddf65755c2ece4747b72c85f1d76cc
ycFan <fyc5860@gmail.com>
2023-09-06 22:53:30 +0800

    feat side-bar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 
-interface IProps {
-  name?: string
-}
+import NavigationSidebar from "@/components/navigation/navigation-sidebar";
 
 const MainLayout = async ({ children }: {children: React.ReactNode}) => { 
   return (
     <div className="h-full">
-      {children}
+      <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+        <NavigationSidebar />
+      </div>
+      <main className="md:pl-[72px] h-full">
+        {children}
+      </main>
+      
     </div>
   )
  }
