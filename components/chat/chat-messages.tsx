9ddb2635b7b83b92777e25d4ee977b6b7f1983cb
ycFan <fyc5860@gmail.com>
2023-09-21 22:32:24 +0800

    feat chat item component

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,10 +1,14 @@
 "use client"
 
+import {format} from 'date-fns'
 import { Member, Message, Profile } from "@prisma/client";
 import { Hash, Loader2, ServerCrash } from "lucide-react";
 import React, { Fragment } from "react";
 import ChatWelcome from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
+import ChatItem from "./chat-item";
+
+const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
 interface ChatMessagesProps {
   name: string;
@@ -74,9 +78,19 @@ const ChatMessages: React.FC<ChatMessagesProps> = (props) => {
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <div key={message.id} >
-                {message.content}
-              </div>
+              <ChatItem 
+                key={message.id}
+                id={message.id}
+                member={message.member}
+                currentMember={member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createAt), DATE_FORMAT)}
+                isUpdated={message.updateAt !== message.createAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery}
+              />
             ))}
           </Fragment>
         ))}
